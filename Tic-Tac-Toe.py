e8b18cb12f1b51391b8bd877a6c1319c853068a8
CUPcqkeee <100496203+Cupcqke@users.noreply.github.com>
2022-10-18 09:55:30 +0300

    Код

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,16 +1,48 @@
-# Говно код снизу
+# Крестики-Нолики
 
 
-game_field = [["_", "_", "_", ],
-              ["_", "_", "_", ],
-              ["_", "_", "_", ]
-              ]
+def fun(line):
+    if line.replace('*', 'x') == 'xxx':
+        return 'x'
+    if line.replace('*', '0') == '000':
+        return '0'
+    return 'x0'
 
 
-def render_field():
-    step = 0
-    for i in game_field:
-        print(i)
-        step += 1
-        if step >= 3:
-            print()
+result = 'x0'
+board = []
+
+n = int(input())
+
+# получаем и сразу проверяем строки
+for _ in range(n):
+    line = input().replace(' ', '')
+    result = fun(line)
+    if result != 'x0':
+        break
+    board.append(line)
+
+# если в строках нет выигрышной комбинации проверяем столбцы
+if result == 'x0':
+    for i in range(n):
+        coll = ''
+        for j in range(n):
+            coll += board[j][i]
+        result = fun(coll)
+        if result != 'x0':
+            break
+
+# если в столбцах нет выигрышной комбинации проверяем диагонали
+if result == 'x0':
+    coll = ''
+    for i in range(n):
+        coll += board[i][i]
+    result = fun(coll)
+
+if result == 'x0':
+    coll = ''
+    for i in range(n):
+        coll += board[i][-1 - i]
+    result = fun(coll)
+
+print(result)
